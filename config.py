25e775593b2d1d4f0e159978fc4ac3542e96450a
Varunkumaran522 <vk9623635@gmail.com>
2025-07-21 13:18:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-ADMIN = int(environ.get("ADMIN", ""))          
-CAPTION = environ.get("CAPTION", "")
+API_ID = int(environ.get("API_ID", "25943549"))
+API_HASH = environ.get("API_HASH", "bd7960c74ffdff6f8e84328aa7f2d7da")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8174304470:AAF49E9HNuPk7nMZnxU0EwPkDDNQSXvQEmc")
+ADMIN = int(environ.get("ADMIN", "6300548815"))          
+CAPTION = environ.get("CAPTION", "Looks like you've been accepted into Sinful Panels... behave yourself, okay? 🌸")
 
 # for thumbnail ( back end is MrMKN brain 😉)
 DOWNLOAD_LOCATION = "./DOWNLOADS"
